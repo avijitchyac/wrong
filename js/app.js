3cfc17c821ae9122856973f14cbf73805440c8ac
Avi Jit <92817867+avijitchyac@users.noreply.github.com>
2022-12-05 06:35:30 +0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,31 @@
+// document.write('<h1>Hello World</h1>');
+// window.alert(123456)
+// console.log('console')
+
+
+//Data Type
+
+// -> String ('', "", ``)
+// document.write(`jhkhkjlkjdslf`)
+
+//-> Number/Integer
+
+// document.write(123456789)
+
+//-> Boolean(true, false)
+
+// document.write(true)
+
+//-> Undefined
+// document.write(undefined)
+
+//-> Null
+// document.write(null)
+
+//Array
+//Object
+
+
+var teacherName = 'Md. Zahid Hossain';
+
+document.write(teacherName);
